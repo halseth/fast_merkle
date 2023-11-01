ce0f1d0ba6ab8c3cc6d6b6e579fccb850908af18
Johan T. Halseth <johanth@gmail.com>
2023-11-01 12:54:50 +0100

    calc merkle tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use std::collections::VecDeque;
 use std::error::Error;
 use std::fmt;
-use std::hash::Hasher;
+use sha2::{Sha256, Digest};
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 pub struct Tree {
+    size: usize,
     leaves: Vec<Vec<u8>>,
     tree: Vec<[u8; 32]>,
 }
@@ -31,14 +33,74 @@ impl Tree {
                 message: "not power of two".to_string(),
             });
         }
-        Ok(Self {
-            leaves: vec![vec![]; size],
-            tree: vec![],
-        })
+        let tree: Vec<[u8; 32]> = vec![[0; 32]; 2*size-1];
+        let leaves = vec![vec![]; size];
+        let mut s = Self {
+            size,
+            leaves,
+            tree,
+        };
+
+        s.commit();
+        Ok(s)
+    }
+
+    pub fn commit(&mut self) -> [u8; 32]{
+        // Keep track of indexes to update.
+        let mut updates: VecDeque<usize> = VecDeque::new();
+
+        // Iterate leaves.
+        for (i, leaf) in self.leaves.iter().enumerate() {
+            let mut hasher = Sha256::new();
+            hasher.update(leaf);
+            let hash = hasher.finalize();
+            let hash_array: [u8; 32] = hash.into();
+            let node= self.size-1+i;
+            self.tree[node] = hash_array;
+
+            println!("leaf {}={:x?} hash={:x?}", i, leaf, hash_array);
+
+            // Push parent node to updates.
+            if i%2 == 0 {
+                updates.push_back((node-1)/2)
+            }
+        }
+
+        // Iterate updates.
+        let mut last_update: usize = 0;
+        while !updates.is_empty() {
+            let node = updates.pop_front().unwrap();
+
+            // Skip double updates in case both children were updated.
+            if node == last_update {
+                continue;
+            }
+
+            let child0 = self.tree[2*node+1];
+            let child1 = self.tree[2*node+2];
+
+            let mut hasher = Sha256::new();
+            hasher.update(child0);
+            hasher.update(child1);
+            let hash = hasher.finalize();
+            let hash_array: [u8; 32] = hash.into();
+
+            self.tree[node] = hash_array;
+            last_update = node;
+
+            // Push parent node to updates.
+            if node != 0 {
+                updates.push_back((node- 1) / 2)
+            }
+        }
+
+
+        // Return new root.
+        self.tree[0]
     }
 
     pub fn root(&self) -> [u8; 32] {
-        return [0; 32];
+        self.tree[0]
     }
 }
 
@@ -64,11 +126,17 @@ mod tests {
 
     #[test]
     fn new() {
-        let tree = Tree::new(4).unwrap();
-        assert_eq!(tree.leaves.len(), 4);
+        let size = 4;
+        let tree = Tree::new(size).unwrap();
+        assert_eq!(tree.leaves.len(), size);
+
+        // Internal tree is double the size of leaves-1.
+        assert_eq!(tree.tree.len(), 2*size-1);
+
+        let zero_root_str = "5310a330e8f970388503c73349d80b45cd764db615f1bced2801dcd4524a2ff4";
+        let zero_root: [u8; 32] = hex::decode(zero_root_str).unwrap().try_into().unwrap();
 
         let root = tree.root();
-        let exp: [u8; 32] = [0; 32];
-        assert_eq!(root, exp);
+        assert_eq!(root, zero_root);
     }
 }
