cc885adca0817b80fd9ed99b3fb522e8d890abe0
Johan T. Halseth <johanth@gmail.com>
2023-12-13 10:29:34 +0100

    test: add test on large tree

--- src/lib.rs
+++ src/lib.rs
@@ -296,6 +296,140 @@ mod tests {
     }
 
     #[test]
+    fn equal_trees() {
+        let size = 67108864;
+
+        let zero_val: Vec<u8> = vec![];
+        let mut tree = Tree::new_with_default(size, zero_val.clone()).unwrap();
+
+        let zero_root_str = "1a3cbd2b72d446c559e6bf49e8bccd22546e127baa2d7f559159d851d77482ae";
+        let zero_root: [u8; 32] = hex::decode(zero_root_str).unwrap().try_into().unwrap();
+        let root = tree.root();
+        assert_eq!(root, zero_root);
+
+        let vals: Vec<(usize, Vec<u8>)> = vec![
+            (16128, vec![127, 69, 76, 70]),
+            (16129, vec![1, 1, 1]),
+            (16132, vec![2, 0, 243, 0]),
+            (16133, vec![1]),
+            (16134, vec![116, 0, 1]),
+            (16135, vec![52]),
+            (16136, vec![32, 3]),
+            (16138, vec![52, 0, 32]),
+            (16139, vec![2, 0, 40]),
+            (16140, vec![6, 0, 5]),
+            (16141, vec![3, 0, 0, 112]),
+            (16142, vec![28, 1]),
+            (16145, vec![26]),
+            (16147, vec![4]),
+            (16148, vec![1]),
+            (16149, vec![1]),
+            (16151, vec![0, 0, 1]),
+            (16152, vec![0, 0, 1]),
+            (16153, vec![28, 1]),
+            (16154, vec![28, 1]),
+            (16155, vec![5]),
+            (16156, vec![0, 16]),
+            (16157, vec![151, 33]),
+            (16158, vec![147, 129, 129, 138, 0]),
+            (16159, vec![55, 1, 32]),
+            (16160, vec![19, 1, 1, 64]),
+            (16161, vec![239, 0, 128, 0]),
+            (16162, vec![111, 0, 64, 3]),
+            (16163, vec![19, 1, 1, 254, 0]),
+            (16164, vec![35, 46, 17]),
+            (16165, vec![35, 44, 129, 0]),
+            (16166, vec![19, 4, 1, 2]),
+            (16167, vec![19, 5, 32]),
+            (16168, vec![239, 0, 192, 2]),
+            (16169, vec![35, 36, 164, 254, 0]),
+            (16170, vec![19, 5]),
+            (16171, vec![131, 32, 193, 1]),
+            (16172, vec![3, 36, 129, 1]),
+            (16173, vec![19, 1, 1, 2]),
+            (16174, vec![103, 128, 0]),
+            (16175, vec![147, 5, 129, 1]),
+            (16176, vec![19, 21, 133, 0]),
+            (16177, vec![147, 2]),
+            (16178, vec![115]),
+            (16179, vec![19, 1, 1, 253, 0]),
+            (16180, vec![35, 38, 129, 2]),
+            (16181, vec![19, 4, 1, 3]),
+            (16182, vec![35, 46, 164, 252, 0]),
+            (16183, vec![35, 38, 4, 254, 0]),
+            (16184, vec![111, 0, 192, 1]),
+            (16185, vec![131, 39, 196, 253, 0]),
+            (16186, vec![147, 151, 23]),
+            (16187, vec![35, 46, 244, 252, 0]),
+            (16188, vec![131, 39, 196, 254, 0]),
+            (16189, vec![147, 135, 23]),
+            (16190, vec![35, 38, 244, 254, 0]),
+            (16191, vec![3, 39, 196, 254, 0]),
+            (16192, vec![147, 7, 112]),
+            (16193, vec![227, 208, 231, 254, 0]),
+            (16194, vec![131, 39, 196, 253, 0]),
+            (16195, vec![19, 133, 7]),
+            (16196, vec![3, 36, 193, 2]),
+            (16197, vec![19, 1, 1, 3]),
+            (16198, vec![103, 128, 0]),
+            (50331424, vec![116, 0, 1]),
+        ];
+
+        for (addr, val) in &vals {
+            tree.set_leaf(*addr, (*val).clone().into());
+        }
+
+        let new_root = tree.commit();
+        let init_exp_root_str = "56ac7fc1a6253af0ed6dc28f2ba3150178aaa9ee783de765c45144e3885732ea";
+        let init_exp_root: [u8; 32] = hex::decode(init_exp_root_str).unwrap().try_into().unwrap();
+
+        assert_eq!(new_root, init_exp_root);
+
+        // Alter two of the leaves.
+        let set_vals: Vec<(usize, Vec<u8>)> = vec![
+            (50331395, vec![0x74, 0x20, 0x01]),
+            (50331424, vec![0x78, 0x00, 0x01]),
+        ];
+
+        for (addr, val) in &set_vals {
+            tree.set_leaf(*addr, (*val).clone().into());
+        }
+
+        let new_root = tree.commit();
+        let second_exp_root_str =
+            "00fea9abf22b0119fbf52222912a2fb0f9879bbb77ec1b571040b35b08b97e3c";
+        let second_exp_root: [u8; 32] = hex::decode(second_exp_root_str)
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+        assert_eq!(new_root, second_exp_root);
+
+        // Build the same tree again, but alter the order of operations.
+        let mut second_tree = Tree::new_with_default(size, zero_val.clone()).unwrap();
+        for (addr, val) in vals {
+            second_tree.set_leaf(addr, val.clone().into());
+        }
+
+        let second_root = second_tree.commit();
+        assert_eq!(second_root, init_exp_root);
+
+        let second_set_vals: Vec<(usize, Vec<u8>)> = vec![
+            (50331424, vec![0x74, 0x00, 0x01]),
+            (50331395, vec![0x74, 0x20, 0x01]),
+            (50331424, vec![0x78, 0x00, 0x01]),
+        ];
+
+        for (addr, val) in second_set_vals {
+            second_tree.set_leaf(addr, val.into());
+            second_tree.commit();
+        }
+
+        let second_altered_root = second_tree.root();
+        assert_eq!(second_altered_root, second_exp_root);
+    }
+
+    #[test]
     fn double_alter() {
         let size = 4;
         let mut tree = Tree::new(size).unwrap();
